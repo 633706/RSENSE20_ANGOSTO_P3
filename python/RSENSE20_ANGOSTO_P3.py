cca401b7da9d8770829bb1aabd8fac543b2694ae
633706 <633706@unziar.es>
2021-06-16 21:36:22 +0200

    Entrega 1: Almacenamiento de datos en archivo .txt con datos separados por ; Corrección archivo Python para manejo de excepción ^C para salida de programa y cierre de archivo y puerto serie.

--- python/RSENSE20_ANGOSTO_P3.py
+++ python/RSENSE20_ANGOSTO_P3.py
@@ -1,4 +1,5 @@
 import serial
+
 port = 'COM3'
 baudrate = 115200
 bytesize = serial.EIGHTBITS
@@ -6,7 +7,18 @@ parity = serial.PARITY_NONE
 stopbits = serial.STOPBITS_ONE
 ser = serial.Serial(port=port, baudrate=baudrate, bytesize=bytesize, parity=parity, stopbits=stopbits)
 
-while True:
-    line = ser.readline()
-    print(line.decode('ascii'),end='')
-ser.close()
+fileHandler = open("AccData.txt", "w")
+fileHandler.write("X;Y;Z\n",)
+try: 
+    while True:
+    #    print(ser.readline().decode('ascii'),end='')
+        line = ser.readline().decode('ascii')
+        Acc_str = line.split()
+        AccX = int(Acc_str[0][5:])
+        AccY = int(Acc_str[1][5:])
+        AccZ = int(Acc_str[2][5:])
+    
+        fileHandler.write(str(AccX)+";"+str(AccY)+";"+str(AccZ)+"\n")
+except KeyboardInterrupt:
+    fileHandler.close()
+    ser.close()
